04f5abf0e44305ee6a29f564965b0f3cbc16cc05
BLACKPA1NT <yeo233@gmail.com>
2023-10-20 16:53:11 +0900

    2차 완성

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         accept: 'application/json',
-//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMDcyODcyNTEzZDIxZWIwMWQwZjk3MWJlMjc3ZDI0NyIsInN1YiI6IjY1MmYzY2FlMDI0ZWM4MDBhZWNkYzY5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RzzoQMlGgVCHK84-QzWPEQeYDt2kLHmdId_XaTaIbHU'
-//     }
-// };
-
-// const key = 'b072872513d21eb01d0f971be277d247';
-
-// fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
-
-
-
-function search() {
-    alert('연결완료')
-}
-
-
 const options = {
     method: 'GET',
     headers: {
@@ -28,47 +6,46 @@ const options = {
     }
 };
 
-const key = 'b072872513d21eb01d0f971be277d247';
 
-fetch('https://api.themoviedb.org/3/movie/popular?api_key=b072872513d21eb01d0f971be277d247&language=ko-KR', options)
-    .then(response => response.json())
-    .then(data => {
-        const movies = data.results;
-        // const movieInfor = document.getElementById('move-infor');
-        // const movieVote = document.getElementById('move-vote');
-        const movieContainer = document.getElementById('movie-container');
+const search_form = document.querySelector('#search_form')
+search_form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    fetchMovies(e.target[0].value);
+})
 
-
-
-        movies.forEach(movie => {
-            // movieInfor.textContent = movie.title;
-            // const movieHtml = `<div>${movie.title}</div>`
-            const movieHtml = `
+function fetchMovies(keyword = '') {
+    fetch(
+        "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
+        options,
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            const movies = data.results;
+            console.log(movies);
+            const movieContainer = document.getElementById("movie-container");
+            movieContainer.innerHTML = '';
+            movies
+                .filter((value) => value.title.includes(keyword))
+                .forEach((movie) => {
+                    const movieHtml = `
             <div class='card' onclick='getMovieId(${movie.id})'>
                 <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} Poster">
                 <h2>${movie.title}</h2>
                 <p class = 'overview'>${movie.overview}</p>
-                
+    
                 <p class = 'vote'>평균 평점: ${movie.vote_average}</p>
             </div>`;
+                    const movieContainer = document.getElementById("movie-container");
+                    movieContainer.innerHTML += movieHtml;
+                });
+        })
+        .catch((err) => console.error(err));
+}
 
-            const movieContainer = document.getElementById('movie-container');
-            movieContainer.innerHTML += movieHtml;
-            // movieVote.textContent = movie.vote_average;
-            // // console.log(movie.title);
-            // console.log(moviehtml);
-            // console.log(movie.overview);
-            // console.log(movie.poster_path);
-            // console.log(movie.vote_average);
-        });
-
-    })
-    .catch(err => console.error(err));
+fetchMovies();
 
 function getMovieId(movieId) {
     alert(`영화 ID : ${movieId}`);
 }
-
-
 
 
